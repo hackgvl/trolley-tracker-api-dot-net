4a6885b3cf8b7d8ad7d15a7660bcf408365de628
Mike Nice <niceman@att.net>
2016-04-15 12:01:46 -0400

    Deployment corrections - allow data deletion for foreign key / index changes, handle existing .woff* MIME without error

--- TrolleyTracker/Migrations/Configuration.cs
+++ TrolleyTracker/Migrations/Configuration.cs
@@ -13,6 +13,7 @@ namespace TrolleyTracker.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(TrolleyTracker.Models.TrolleyTrackerContext context)
